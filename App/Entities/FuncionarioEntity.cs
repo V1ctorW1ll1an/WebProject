041f45f287f9ad340e09de2175faed009374b350
Victor Willian <victorwillian1012@gmail.com>
2023-06-16 20:50:22 -0300

    chore: :white_check_mark: Add Cpf in FuncionarioEntity  Fixes #28

--- App/Entities/FuncionarioEntity.cs
+++ App/Entities/FuncionarioEntity.cs
@@ -4,11 +4,19 @@ public class FuncionarioEntity
 {
     public FuncionarioEntity() { }
 
-    public FuncionarioEntity(int? id, string nome, string email, string? senha, CargoEntity cargo)
+    public FuncionarioEntity(
+        int? id,
+        string nome,
+        string email,
+        int cpf,
+        string? senha,
+        CargoEntity cargo
+    )
     {
         Id = id;
         Nome = nome;
         Email = email;
+        Cpf = cpf;
         Senha = senha;
         Cargo = cargo;
     }
@@ -16,6 +24,7 @@ public class FuncionarioEntity
     public int? Id { get; set; }
     public string Nome { get; set; } = null!;
     public string Email { get; set; } = null!;
+    public int Cpf { get; set; }
     public string? Senha { get; set; }
     public CargoEntity Cargo { get; set; } = null!;
 }
